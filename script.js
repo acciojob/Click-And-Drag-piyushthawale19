4a8c84f4afee61441a5b2bdcf841a025d9219db1
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-07-21 23:47:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrolleLeft;
+
+slider.addEventListener('mousedown', (e)=>{
+	isDown= true;
+	slider.classList.add('active');
+	startX = e.pageX - slider.offsetLeft;
+	scrolleLeft = slider.scrolleLeft;
+})
+
+slider.addEventListener('mouseleave', ()=>{
+	isDown= false;
+	slider.classList.remove('active')
+})
+
+slider.addEventListener('mouseup',()=>{
+	isDown = false;
+  slider.classList.remove('active');
+})
+
+slider.addEventListener('mousemove',(e)=>{
+	if(!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk =(x -startX)*1.5;
+	slider.scrolleLeft = scrolleLeft - walk;
+})
